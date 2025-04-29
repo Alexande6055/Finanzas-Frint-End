f9ede82f330feba6d52000e1192a1036b38eced3
Alexander <juanperzezss@gmail.com>
2025-04-28 23:34:43 -0500

    refactorisacion front-end

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist' // Aquí lo defines explícitamente
-  }
+  },
+  base: '/', // Configuración importante para que funcione el enrutamiento correctamente
 })
